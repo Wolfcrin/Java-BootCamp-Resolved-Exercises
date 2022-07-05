ff698ab9b3bdb0bd13e4330168f363ef558a69d0
Racelom <pugli88@gmail.com>
2022-07-05 20:25:39 -0300

    M1-Introduction-to-Java
    Print Your Initials in console

---
+++ Module 1 - Introduction to Java/1. Getting Started/Challenge 1/YourInitials.java
@@ -0,0 +1,12 @@
+public class YourInitials {
+    public static void main(String[] args) {
+        //print initials here.
+        System.out.println(" CCC   SSS   PPPPP");
+        System.out.println("C     S   S  P   P");
+        System.out.println("C     S      P   P");
+        System.out.println("C      SSS   PPPPP");
+        System.out.println("C         S  P    ");
+        System.out.println("C     S   S  P    ");
+        System.out.println(" CCC   SSS   P    ");
+    }
+}
