88b34ce713cc32f902e1a88d60f29f34c305c59b
Racelom <pugli88@gmail.com>
2022-07-05 20:41:33 -0300

    M1 -Variables - Workbook 2.2 - Biografy
    Print a simple biography using different types of variables.

---
+++ Module 1 - Introduction to Java/2. Variables/Workbook 2.2/Biography.java
@@ -0,0 +1,27 @@
+public class Biography {
+    public static void main(String[] args) {
+
+        //make a name variable
+        String name = "Harry";
+        //make an age variable
+        int years = 12;
+        //make a country variable
+        String country ="Argentine";
+        //make a sport variable
+        String sport ="Football";
+        //make an hours variable
+        int hoursHePlaySport = 2;
+        //make a game variable
+        String favoriteGame = "Chess";
+        //make a subject variable
+        String subject ="History";
+        //make a grade variable
+        String grade = "2B";
+
+        System.out.println("My name is "+name+". I'm <age> years old, and I'm from "+ country);
+        System.out.println("My favourite sport is "+sport+". I play for "+hoursHePlaySport+" hours a day");
+        System.out.println("When I'm tired, I like to play "+favoriteGame+".");
+        System.out.println("In school, my favourite subject was "+subject+", I scored a "+grade+".");
+
+    }
+}
